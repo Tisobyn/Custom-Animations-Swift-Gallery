8ca73cde65e7982bd9c2f64fd99d8f474e70aaae
Tiso <tisobyn@gmail.com>
2024-09-01 21:20:38 +0500

    Initial Commit

---
+++ Custom Animations Swift Gallery/Custom_Animations_Swift_GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Animations_Swift_GalleryApp.swift
+//  Custom Animations Swift Gallery
+//
+//  Created by Yermek Sabyrzhan on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Animations_Swift_GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
